ab9cd7886339908620d090b7aecc8b8414289742
91876 <91876@host.docker.internal>
2024-11-06 10:53:44 +0530

    create by kalu

---
+++ GitHubDemo/src/main/java/com/kcpit/GitHubDemo.java
@@ -0,0 +1,10 @@
+package com.kcpit;
+
+public class GitHubDemo {
+
+	public static void main(String[] args) {
+		System.out.println("GitHubDemo.main()");
+
+	}
+
+}
---
+++ GitHubDemo/src/main/java/com/kcpit/controller/GithHubController.java
@@ -0,0 +1,10 @@
+package com.kcpit.controller;
+
+public class GithHubController {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
